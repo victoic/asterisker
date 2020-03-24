aac7b31ee0670fdbbd76372f0e312f44c27c3f69
Victor Lundgren <victor15_toic@hotmail.com>
2020-03-24 13:48:23 -0300

    Added tweet button and char counter

--- app/static/js/asterisker.js
+++ app/static/js/asterisker.js
@@ -1,14 +1,54 @@
 function asterisker(){
   string = document.getElementById('input_text').value
-  keep_string = " \n!@#$%¨&*()¹²³£¢¬´`{[ª~^]}º,<.>;:/?°\\|1234567890-_=+§"
+  keep_string = " \n!@#$%¨&*()¹²³£¢¬´`\"'{[ª~^]}º,<.>;:/?°\\|1234567890-_=+§"
+  url_enconding = {
+    "%":"%25",
+    "\n":"%0A",
+    "!":"%21",
+    "#":"%23",
+    "$":"%24",
+    "&":"%26",
+    "'":"%27",
+    "(":"%28",
+    ")":"%29",
+    "+":"%2B",
+    "/":"%2F",
+    ";":"%3B",
+    "?":"%3F",
+    "\"":"%22",
+    "\\":"%5C",
+    "^":"%5E",
+    "{":"%7B",
+    "}":"%7C",
+    "|":"%7C",
+    "°":"%C2%BA",
+    "º":"%C2%B0",
+    "ª":"%C2%AA",
+    "´":"%C2%B4",
+    "`":"%60"
+  }
+
   for (var i = 0; i < string.length; i++){
     char = string.charAt(i)
     if (keep_string.indexOf(char) < 0){
       string = string.replace(char,'*')
     }
   }
+
+  var url_string = string
+  for (var key in url_enconding){
+    if (string.indexOf(key) > -1) {
+      url_string = url_string.split(key).join(url_enconding[key])
+    }
+  }
+
   document.getElementById('asterisked').value = string
   document.getElementById('asterisked-info').style.visibility='visible'
+
+  document.getElementById('tweetCounter').innerHTML ="Twitter limit: "+string.length+"/267"
+  
+  var url = "https://twitter.com/intent/tweet?button_hashtag=asterisker&ref_src=twsrc%5Etfw&text=" + url_string + "%0A"
+  document.getElementById("tweet_btn").href = url
   $('#copy-button').popover(); 
 }
 
@@ -18,3 +58,7 @@ function copyText(){
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
 }
+
+window.onload = function() {
+  document.getElementById('tweetCounter').innerHTML ="Twitter limit: "+document.getElementById('asterisked').value.length+"/267"
+};
